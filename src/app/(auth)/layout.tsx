ba98c8bf031fdfbab9788388e5fb75cbc1af50ea
Cameron Hake <cameron.hake@gmail.com>
2023-09-24 16:32:45 -0500

    change middleware and signin/up images

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
       <AspectRatio ratio={16 / 9}>
         <Image
-          src="/images/signin-pic.webp"
+          src="/images/newLogin.webp"
           alt="A skateboarder doing a high drop"
           fill
           className="absolute inset-0 object-cover"
